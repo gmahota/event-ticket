ddde09b5c83c0135ac50f7de88440bd2a769eea2
Guimarães Mahota <guimaraesmahota@gmail.com>
2022-02-06 21:06:10 +0200

    tailwind config

---
+++ client/tailwind/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+  night: "#121212",
+  dusk: "#202022",
+  dawn: "#8A8F98",
+  sunrise: "#F7F8F8",
+  zenith: "#ffffff",
+  purble: "#515cc3",
+}
+// eslint-disable-next-line no-undef
+module.exports = colors
